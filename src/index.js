5bb275f27409cdd5cc67f1f888de7052a2e1c3f0
FJiang6 <jiangfan6@outlook.com>
2018-12-08 22:44:23 +0800

    react demo 01

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>现在是 {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('example')
+    );
+}
+
+setInterval(tick, 1000);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
